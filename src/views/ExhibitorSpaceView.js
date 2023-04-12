de3d90bab7f5e84778c962a9de410255cdcac93f
0rigin-All <113370094+0rigin-All@users.noreply.github.com>
2023-04-12 13:38:54 +0200

    Update ExhibitorSpaceView.js

--- src/views/ExhibitorSpaceView.js
+++ src/views/ExhibitorSpaceView.js
@@ -1,9 +1,11 @@
 import LoginView from "./LoginView";
 import ExhibitopSpaceConnectedView from "./ExhibitorSpaceConnectedView";
+import {useContext} from "react";
+import Auth from "../contexts/Auth";
 
 export default function ExhibitopSpaceView(props) {
-  const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn) {
+  const {isAuthentificated}=useContext(Auth);
+  if (isAuthentificated) {
     return <ExhibitopSpaceConnectedView />;
   }
   return <LoginView />;
